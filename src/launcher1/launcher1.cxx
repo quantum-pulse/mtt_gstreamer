700bca75986ec851c9d4093bd27898d5f013a49c
Robert King <robertk@maunakeatech.com>
2020-08-27 00:53:10 +0200

    initial commit

---
+++ src/launcher1/launcher1.cxx
@@ -0,0 +1,37 @@
+
+#include <tools>
+#include <gst/gst.h>
+#include <string>
+
+using namespace std;
+
+string gBuffer="playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+
+int main(int argc,char ** argv)
+{
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+
+    //Initialize GStreamer 
+    gst_init (&argc, &argv);
+    // Build the pipeline 
+    pipeline = gst_parse_launch(gBuffer.c_str(),nullptr);
+
+    // Start playing 
+    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+    // Wait until error or EOS 
+    bus = gst_element_get_bus (pipeline);
+    auto lgst_err = GST_MESSAGE_ERROR | GST_MESSAGE_EOS;
+    msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,(GstMessageType)lgst_err) ;//GST_MESSAGE_ANY;
+    
+    // Free resources 
+    if (msg != nullptr)
+        gst_message_unref (msg);
+    gst_object_unref (bus);
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+    
+    return 0;
+}
---
+++ src/launcher3/launcher3.cxx
@@ -0,0 +1,87 @@
+
+#include <tools>
+#include <gst/gst.h>
+#include <string>
+
+using namespace std;
+
+string gBuffer="playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+
+int main(int argc,char ** argv)
+{
+    GstElement *pipeline, *source, *sink;
+    GstElement * filter;
+    GstBus *bus;
+    GstMessage *msg;
+    GstStateChangeReturn ret;
+
+    /* Initialize GStreamer */
+    gst_init (&argc, &argv);
+
+    /* Create the elements */
+    source = gst_element_factory_make ("videotestsrc", "source");
+    sink = gst_element_factory_make ("autovideosink", "sink");
+    filter = gst_element_factory_make ("vertigotv", "filter");
+
+    /* Create the empty pipeline */
+    pipeline = gst_pipeline_new ("test-pipeline");
+
+    if (!pipeline || !source || !sink || !filter) {
+        g_printerr ("Not all elements could be created.\n");
+        return -1;
+    }
+
+    /* Build the pipeline */
+    gst_bin_add_many (GST_BIN (pipeline), source, filter , sink, nullptr);
+    if (gst_element_link (source, sink) != TRUE || gst_element_link (filter, sink) != TRUE ) {
+        g_printerr ("Elements could not be linked.\n");
+        gst_object_unref (pipeline);
+        return -1;
+    }
+
+    /* Modify the source's properties */
+    g_object_set (source, "pattern", 0, nullptr);
+
+    /* Start playing */
+    ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    if(ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr ("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref (pipeline);
+        return -1;
+    }
+
+    /* Wait until error or EOS */
+    bus = gst_element_get_bus (pipeline);
+    auto lGst_err = GST_MESSAGE_ERROR | GST_MESSAGE_EOS;
+    msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, (GstMessageType)lGst_err);
+
+    /* Parse message */
+    if(msg != nullptr){
+        GError *err;
+        gchar *debug_info;
+
+        switch (GST_MESSAGE_TYPE (msg)) {
+        case GST_MESSAGE_ERROR:
+            gst_message_parse_error (msg, &err, &debug_info);
+            g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+            g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+            g_clear_error (&err);
+            g_free (debug_info);
+            break;
+        case GST_MESSAGE_EOS:
+            g_print ("End-Of-Stream reached.\n");
+            break;
+        default:
+            /* We should not reach here because we only asked for ERRORs and EOS */
+            g_printerr ("Unexpected message received.\n");
+            break;
+        }
+        gst_message_unref (msg);
+    }
+
+    /* Free resources */
+    gst_object_unref (bus);
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+    return 0; 
+}
